e521792d6415066571af8a030f0b9c271a2b399b
tylermccreary <tjmccreary@gmail.com>
2018-02-17 17:19:36 -0800

    Added portal scene. Have not implemented moving through the portal yet

---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerCamera.position;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        /*Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);*/
+        /*transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);*/
+    }
+}
