1bec2f18d4d09b01e4216c32f0a3e5685506b288
Russell Alleen-Willems <ralleenwillems@gmail.com>
2018-02-24 15:29:38 -0800

    Added Mapbox terrain generation to scene "test_Mapbox". This could be a good scene to duplicate as the base for our individual time scenes.

---
+++ Assets/Scripts/Movement/ControllerInputManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputManager : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+    //Grabbing and Throwing
+    public float throwForce = 1.5f;
+
+    //Anticheat
+    //public AntiCheat antiCheat;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        Debug.Log("Your trigger is inside " + col.gameObject.name);
+        if (col.gameObject.CompareTag("Throwable"))
+        {
+            /*if (antiCheat.isOutOfBounds)
+            {
+                Debug.Log("Enabling Anti_cheat mode");
+                antiCheat.toggleCheatMode(true);
+            }
+            else
+            {
+                Debug.Log("Disabling Anti_cheat mode");
+                antiCheat.toggleCheatMode(false);
+            }*/
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                ThrowObject(col);
+            }
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(col);
+            }
+        }
+        else if (col.gameObject.CompareTag("Structure"))
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                PlaceObject(col);
+            }
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(col);
+            }
+
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    }
+
+    void PlaceObject(Collider coli)
+    {
+        coli.transform.SetParent(null); // unparent the object from controller
+        Rigidbody rigidBody = coli.GetComponent<Rigidbody>();
+        rigidBody.isKinematic = true; // re-enable physics on object.
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+        Debug.Log("Placed object " + coli.gameObject.name);
+    }
+
+    void ThrowObject(Collider coli)
+    {
+        coli.transform.SetParent(null);
+        Rigidbody rb = coli.GetComponent<Rigidbody>();
+        if (coli.GetComponent<Rigidbody>())
+        {
+            rb.isKinematic = false; // re-enable physics on object
+        }
+
+        rb.velocity = device.velocity * throwForce; // add throw force and vectors/velocities from controller
+        rb.angularVelocity = device.angularVelocity;
+        Debug.Log("You have thrown the " + coli.gameObject.name);
+    }
+
+    void GrabObject(Collider coli)
+    {
+        coli.transform.SetParent(gameObject.transform);
+        coli.GetComponent<Rigidbody>().isKinematic = true; // Stop physics from acting on the object
+        device.TriggerHapticPulse(2000);
+        Debug.Log("You have grabbed the " + coli.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Movement/HandControllerInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControllerInput : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+    // Teleporter
+    private LineRenderer laser;
+    public GameObject teleportAimerObject;
+    public Vector3 teleportLocation;
+    public GameObject player;
+    public LayerMask laserMask;
+    public float yNudgeAmount = 0.0f; //specific to teleportAimerObject height
+
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        laser = GetComponentInChildren<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            laser.gameObject.SetActive(true);
+            teleportAimerObject.SetActive(true);
+
+            //set start pos of laser
+            laser.SetPosition(0, gameObject.transform.position);
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, 15, laserMask))
+            {
+                teleportLocation = hit.point;
+                laser.SetPosition(1, teleportLocation);
+                //aimer position
+                teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+            }
+            else
+            {
+                teleportLocation = new Vector3(transform.forward.x * 15 + transform.position.x, transform.forward.y * 15 + transform.position.y, transform.forward.z * 15 + transform.position.z);
+                RaycastHit groundRay;
+                if(Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 17, laserMask))
+                {
+                    teleportLocation = new Vector3(transform.forward.x * 15 + transform.position.x, groundRay.point.y, transform.forward.z * 15 + transform.position.z);
+                }
+                //aimer position
+                teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+                laser.SetPosition(1, teleportAimerObject.transform.position);
+            }
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            laser.gameObject.SetActive(false);
+            teleportAimerObject.SetActive(false);
+            player.transform.position = teleportLocation;
+        }
+	}
+}
